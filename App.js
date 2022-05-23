614b628d189da5448b6b12f31b2325241aabe67a
audisiofilho <obuzua46@gmail.com>
2022-05-23 19:55:27 -0300

    Criando Routes e adicioando algumas pages

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+
+import {NavigationContainer} from '@react-navigation/native';
+import Routes from './src/routes';
 
 export default function heyGroupsApp() {
- return (
-   <View>
-     <Text>Ola</Text>
-     </View>
+  return (
+    <NavigationContainer>
+      <Routes />
+    </NavigationContainer>
   );
 }
