0f31f23d87c93e1e48006a2cdd1f24cd4224559c
audisiofilho <obuzua46@gmail.com>
2022-05-25 11:02:46 -0300

    Adicionando Status Bar

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {StatusBar} from 'react-native';
 
 import {NavigationContainer} from '@react-navigation/native';
 import Routes from './src/routes';
@@ -6,6 +7,7 @@ import Routes from './src/routes';
 export default function heyGroupsApp() {
   return (
     <NavigationContainer>
+      <StatusBar backgroundColor="#179bd7"/>
       <Routes />
     </NavigationContainer>
   );
