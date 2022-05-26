e915616423ee9b989b01b07fb94fec5177bab40d
audisiofilho <obuzua46@gmail.com>
2022-05-26 18:15:39 -0300

    Criando Input de enviar mensagem

--- src/pages/Messages/index.js
+++ src/pages/Messages/index.js
@@ -1,14 +1,29 @@
 import React, {useState, useEffect} from 'react';
-import {View, Text, FlatList, SafeAreaView} from 'react-native';
+import {
+  FlatList,
+  SafeAreaView,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableOpacity,
+} from 'react-native';
 
 import auth from '@react-native-firebase/auth';
 import firestore from '@react-native-firebase/firestore';
 
 import ChatMessage from '../../components/ChatMessage';
 
+import Feather from 'react-native-vector-icons/Feather';
+import {
+  ButtonContainer,
+  ContainerInput,
+  MainContainerInput,
+  TextInput,
+} from './styles';
+
 export default function Messages({route}) {
   const {thread} = route.params;
   const [messages, setMessages] = useState([]);
+  const [input, setInput] = useState();
 
   const user = auth().currentUser.toJSON();
   useEffect(() => {
@@ -52,6 +67,28 @@ export default function Messages({route}) {
         showsVerticalScrollIndicator={false}
         style={{width: '100%'}}
       />
+
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={{width: '100%'}}
+        keyboardVerticalOffset={100}>
+        <ContainerInput>
+          <MainContainerInput>
+            <TextInput
+              placeholder="Digite sua mensagem..."
+              value={input}
+              onChangeText={text => setInput(text)}
+              multiline={true}
+              autoCorrect={false}
+            />
+          </MainContainerInput>
+          <TouchableOpacity>
+            <ButtonContainer>
+              <Feather name="send" size={22} color="#fff" />
+            </ButtonContainer>
+          </TouchableOpacity>
+        </ContainerInput>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 }
---
+++ src/pages/Messages/styles.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components/native';
+
+export const ContainerInput = styled.View`
+  flex-direction: row;
+  margin: 10px;
+  align-items: center;
+`;
+export const MainContainerInput = styled.View`
+  flex-direction: row;
+  align-items: center;
+  background-color: #fff;
+  flex: 1;
+  border-radius: 25px;
+  margin-right: 10px;
+`;
+
+export const TextInput = styled.TextInput`
+  flex: 1;
+  margin-left: 10px;
+  margin-right: 10px;
+  max-height: 130px;
+  min-height: 48px;
+`;
+
+export const ButtonContainer = styled.View`
+  background-color: #51c880;
+  height: 48px;
+  width: 48px;
+  border-radius: 25px;
+  align-items: center;
+  justify-content: center;
+`;
